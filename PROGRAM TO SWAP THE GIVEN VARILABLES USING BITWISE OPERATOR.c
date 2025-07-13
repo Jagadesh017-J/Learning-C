506ac6c15ab7b79df027ddd2ce2c91be4335a3b7
Jagadesh J <jagadeshj563@gmail.com>
2025-07-13 21:59:33 +0530

    Create  PROGRAM TO SWAP THE GIVEN VARILABLES USING BITWISE OPERATOR.c
    This is one of my first practice programs while learning C fundamentals.

---
+++ PROGRAM TO SWAP THE GIVEN VARILABLES USING BITWISE OPERATOR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,temp;
+    printf("enter the number = ");
+    scanf("%d",&num1);
+    printf("enter the number 2 = ");
+    scanf("%d",&num2);
+    num1=(num1^num2)^(num2=num1);
+    printf("num1 = %d\nnum2 = %d\n",num1,num2);
+}
