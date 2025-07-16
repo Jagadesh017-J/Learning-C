dd9803b1685bd5011b48c8fd74e2b4c7da3af9cc
Jagadesh J <jagadeshj563@gmail.com>
2025-07-16 21:30:12 +0530

    Create WRITE A PROGRAM TO PRINT PRIME NUMBER BETWEEN 1 TO 100.c

---
+++ Loop Statements/WRITE A PROGRAM TO PRINT PRIME NUMBER BETWEEN 1 TO 100.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,temp;
+    for(i=1;i<=100;i++)
+    {
+        temp=0;
+        for(j=2;i>j;j++)
+        {
+        if(i%j==0)
+        {
+        temp=1;
+        break;
+        }
+            
+        }
+    if(temp==0)
+    printf("%d\n",i);
+    }
+}
