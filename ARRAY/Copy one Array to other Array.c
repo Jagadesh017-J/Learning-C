91889b4b80963f13e2ec66a946db80c428a245f6
Jagadesh J <jagadeshj563@gmail.com>
2025-07-27 15:03:10 +0530

    Create Copy one Array to other Array.c

---
+++ ARRAY/Copy one Array to other Array.c
@@ -0,0 +1,15 @@
+//Copy one Array to other Array
+#include <stdio.h>
+
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int b[5],i;
+    for(i=0;i<5;i++)
+    {
+        b[i]=a[i];
+    }
+    printf("B sting :\n");
+    for(i=0;i<5;i++)
+    printf("%d",b[i]);
+}
