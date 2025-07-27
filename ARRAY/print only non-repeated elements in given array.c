d561ea8df315421475d0390b08193d92d63800d6
Jagadesh J <jagadeshj563@gmail.com>
2025-07-27 14:58:27 +0530

    Create print only non-repeated elements in given array.c

---
+++ ARRAY/print only non-repeated elements in given array.c
@@ -0,0 +1,26 @@
+//print only non-repeated elements in given array
+#include <stdio.h>
+
+int main()
+{
+	int a[] = {4, 2, 4, 5, 2, 3, 5, 1},i,j,c;  // Example array
+	int n = sizeof(a) / sizeof(a[0]);
+	  for(i=0;i<n;i++)
+        {
+                for(j=0;j<i;j++) {
+                 if(a[i]==a[j])
+                 break;
+                }
+                if(j==i)
+                {
+                        for(j=i+1,c=1;j<n;j++)
+                        if(a[i] == a[j])
+                        c++;
+
+                        if(c==1)
+                        printf("%d  ",a[i]);
+                }
+        }
+
+
+}
